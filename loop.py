896d15ddc731f0af9ecc00d1cca6d0c51bd56a4b
Aman Gautam <amangautam2021ag@gmail.com>
2025-01-19 00:14:35 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,44 @@
+i =1
+while i <= 5:
+    print("hello")
+    i +=1
+print(i)     # print last number of ending 4
+
+
+""" Print number from 1 to 100 """
+i = 1
+while i <= 100 :
+    print(i)
+    i = i+1
+
+"""Print number from 100 to 1 """
+i = 100
+while i >= 1:
+  print(i)
+  i = i-1
+
+"""print the multiplication number of n """
+
+n = int(input("Enter the number they you want print table :- "))
+i = 1
+while i<= 10:
+  print(n*i)
+  i+=1
+
+"""print the element of the following list """
+
+list = [1,4,9,16,25,36,49,64,81,100]
+idx = 0
+while idx < len(list) :
+  print(list[idx])
+  idx +=1
+
+
+"""print the element of the following list"""
+
+name = ["aman", "addi", "anshu", "ankit", "sam"]
+idx = 0
+while idx < len(name):
+  print(name[idx])
+  idx+=1
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+nput("Enter any light colour :- ")
+if(light == "red"):
+    print("STOP 🔴 ")
+
+elif(light == "yellow"):
+    print("LOOK AHEAD 🟡 ")
+
+elif(light == "green"):
+    print("GO 🟢 ")
+
+else:
+    print("the light is broken")
+
+print("THANK YOUHH!")
+
